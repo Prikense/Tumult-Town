653f97bb7d6fa9adfbf7496bdd840a9253df1146
Prikense <prikense@hotmail.com>
2022-09-01 17:08:44 -0500

    cleaned movement script, added headbobbing

--- Assets/Scripts/FPSMoveRigidBody.cs
+++ Assets/Scripts/FPSMoveRigidBody.cs
@@ -10,7 +10,7 @@ public class FPSMoveRigidBody : MonoBehaviour
     Rigidbody body;
 //    Vector3 velocity;
     //public float grav = 9.8f;
-    private bool grounded;
+    public bool grounded;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -23,11 +23,26 @@ public class FPSMoveRigidBody : MonoBehaviour
     private float time = 0.0f;
 
 
+    //for camera movement when moving
+    public bool BobEnable = true;
+
+    [SerializeField] private float amplitude = 0.15f;
+    [SerializeField] private float frequency = 10.0f;
+
+    [SerializeField] private Transform Whoknows;
+    [SerializeField] private Transform camera;
+//    [SerializeField] private Transform gun;
+
+    private float ToggleSpeed = 3.0f;
+    private Vector3 startPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
         time = dashMulti;
+        startPos = camera.localPosition;
     }
 
     // Update is called once per frame
@@ -64,9 +79,41 @@ public class FPSMoveRigidBody : MonoBehaviour
                     inputXY = inputXY*dashMulti;
                 }
         }
+
+        //camera movement
+        if(!BobEnable){return;}
+        Debug.Log("camera local pos: "+camera.localPosition);
+        Debug.Log("start pos?: "+startPos);
+        
+        CheckMotion();
+        camera.LookAt(FocusTarget());
     }
 
     void FixedUpdate(){
         body.AddRelativeForce(inputXY * speed);
+    }
+
+    private void PlayMotion(Vector3 motion){
+        camera.localPosition +=motion;
+    }
+    private Vector3 FootStep(){
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * frequency) * amplitude;
+        pos.x += Mathf.Cos(Time.time * frequency/2)*amplitude*2;
+        return pos;
+    }
+    private void CheckMotion(){
+        float speed = new Vector3(body.velocity.x, 0, body.velocity.z).magnitude;
+        if (speed < ToggleSpeed || !grounded) {resetPos();return;}
+        PlayMotion(FootStep());
+    }
+    private void resetPos(){
+        if(camera.localPosition == startPos) return;
+        camera.localPosition = Vector3.Lerp(camera.localPosition, startPos, 1 *Time.deltaTime);
+    }
+    private Vector3 FocusTarget(){
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y+Whoknows.localPosition.y, transform.position.z);
+        pos += Whoknows.forward *15f;
+        return pos;
     }
 }
