64d9bbcf6dca8bb5ac03d55c34b1ec8dc771c9af
Prikense <prikense@hotmail.com>
2022-11-29 11:37:48 -0600

    fixed spawn position and rotation

--- Assets/Scripts/Online/ObjectSpawnController.cs
+++ Assets/Scripts/Online/ObjectSpawnController.cs
@@ -13,12 +13,12 @@ public class ObjectSpawnController : MonoBehaviour
     public void SpawnPlayer(NetworkRunner runner, PlayerRef playerRef)
     {
         Debug.Log("Spawn Player");
-        Vector3 spawnPosition = new Vector3(-125, 50, 90+((_networkedObjects.Count-1)*-25));
+        Vector3 spawnPosition = new Vector3(-125, 50, 80+Random.Range(0,15)*2);
         if(_networkedObjects.Count == 0){
-            NetworkObject _object = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, playerRef);
+            NetworkObject _object = runner.Spawn(_playerPrefab, spawnPosition, new Quaternion(0,80,0,0), playerRef);
             _networkedObjects.Add(_object);
         }else{
-            NetworkObject _object = runner.Spawn(_playerPrefab2, spawnPosition, Quaternion.identity, playerRef);
+            NetworkObject _object = runner.Spawn(_playerPrefab2, spawnPosition, new Quaternion(0,80,0,0), playerRef);
             _networkedObjects.Add(_object);
         }
         Debug.Log($"Objects in simulation {_networkedObjects.Count}");
