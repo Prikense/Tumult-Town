25b38da289ce31161b319f0e3a59818ff7746c36
Prikense <a01251446@itesm.mx>
2022-12-01 05:10:32 -0600

    fixed typo on score setting

--- Assets/Scripts/Online/ScoreScriptO.cs
+++ Assets/Scripts/Online/ScoreScriptO.cs
@@ -6,18 +6,18 @@ using Fusion;
 public class ScoreScriptO : NetworkBehaviour
 {
  
-    [Networked] private int netPlayerScore1 {get; set;}
-    [Networked] private int netPlayerScore2 {get; set;}
+    [Networked] public int netPlayerScore1 {get; set;}
+    [Networked] public int netPlayerScore2 {get; set;}
     
 
-    private int _player1Score;
+    [SerializeField] private int _player1Score;
     public int Player1Score
     {
         get{return _player1Score;}
         set{_player1Score = value;}
     }
 
-    private int _player2Score;
+    [SerializeField] private int _player2Score;
 
     public int Player2Score
     {
@@ -37,6 +37,9 @@ public class ScoreScriptO : NetworkBehaviour
         if(Object.Runner.IsServer){
             Player1Score = 1;
             Player2Score = 1;
+        }else{
+            Player1Score = netPlayerScore1;
+            Player2Score = netPlayerScore2;
         }
     }
 
@@ -45,6 +48,9 @@ public class ScoreScriptO : NetworkBehaviour
         if(Object.Runner.IsServer){
             netPlayerScore1 = _player1Score;
             netPlayerScore2 = _player2Score;
+        }else{
+            Player1Score = netPlayerScore1;
+            Player2Score = netPlayerScore2;
         }
     }
 
